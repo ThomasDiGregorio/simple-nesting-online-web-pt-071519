6031ac6fa539e872a4ed2df49b069cbfe50646ad
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-07-18 02:06:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,7 +122,7 @@ def changing_alan
         }
      }
      
-     
+     alans_new_info = "GUI"
      
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
